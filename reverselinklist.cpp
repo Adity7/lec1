3aa57b1d41bc143b0e2541832abcba2dc7b745c4
Adity7 <58148990+Adity7@users.noreply.github.com>
2021-06-29 22:21:01 +0530

    Add files via upload

---
+++ reverselinklist.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode{
+  int val;
+  ListNode *next;
+  ListNode(int x){
+    val = x;
+  }
+};
+
+
+class Solution{
+
+public:
+
+  ListNode reverseList(ListNode* head){
+    ListNode* prev = NULL;
+    while(head!=NULL){
+      ListNode *next = head->next;
+      head->next = prev;
+      prev = head;
+      head = next;
+    }
+
+    return prev;
+
+  }
+
+};
